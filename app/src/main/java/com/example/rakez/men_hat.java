9184ddbbb3df1aeef7168d6b747fd528935c84fb
daad abdulaziz <da3d111@gmail.com>
2022-05-05 16:05:55 +0300

    men tables

---
+++ app/src/main/java/com/example/rakez/men_hat.java
@@ -0,0 +1,14 @@
+package com.example.rakez;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class men_hat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_men_hat);
+    }
+}
---
+++ app/src/main/java/com/example/rakez/men_thoub.java
@@ -0,0 +1,14 @@
+package com.example.rakez;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class men_thoub extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_men_thoub);
+    }
+}
