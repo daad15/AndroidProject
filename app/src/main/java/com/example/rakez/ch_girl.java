0aaa253c048b3a70d7fe4b68a1cee06d92b11474
Raghad209e <>
2022-05-06 21:52:02 +0300

    toddler

---
+++ app/src/main/java/com/example/rakez/ch_girl.java
@@ -0,0 +1,14 @@
+package com.example.rakez;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ch_girl extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ch_girl);
+    }
+}
