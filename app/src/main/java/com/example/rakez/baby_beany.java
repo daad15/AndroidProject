d4e9273d948f6cec3ac953a55db4bac81d353578
daad abdulaziz <da3d111@gmail.com>
2022-05-06 20:08:24 +0300

    baby beany

---
+++ app/src/main/java/com/example/rakez/baby_beany.java
@@ -0,0 +1,14 @@
+package com.example.rakez;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class baby_beany extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_baby_beany);
+    }
+}
