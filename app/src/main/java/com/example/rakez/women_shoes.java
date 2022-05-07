b6d1f4496e9583075b0c5d42496479d932b897d7
Raghad209e <>
2022-05-07 06:52:31 +0300

    women_bra

---
+++ app/src/main/java/com/example/rakez/women_shoes.java
@@ -0,0 +1,14 @@
+package com.example.rakez;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class women_shoes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_women_shoes);
+    }
+}
