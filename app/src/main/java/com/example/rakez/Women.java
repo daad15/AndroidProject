933d7f64757f596d6ebd458180fa933b36c31f6d
KadiAlrwaishdi <kadia654@gmail.com>
2022-05-09 03:05:00 +0300

    women page manifest edited(pages are added)

--- app/src/main/java/com/example/rakez/Women.java
+++ app/src/main/java/com/example/rakez/Women.java
@@ -20,6 +20,79 @@ public class Women extends AppCompatActivity {
         startActivity(i);
         finish();
     }
+
+
+    public void go_Wabaya(View V){
+        Intent i = new Intent(this,women_abaya.class);
+        startActivity(i);
+        finish();
+    }
+
+    public void go_Wtop(View V){
+        Intent i = new Intent(this,women_top.class);
+        startActivity(i);
+        finish();
+    }
+
+    public void go_Wpants(View V){
+        Intent i = new Intent(this,women_pants.class);
+        startActivity(i);
+        finish();
+    }
+
+    public void go_Wbra(View V){
+        Intent i = new Intent(this,women_bra.class);
+        startActivity(i);
+        finish();
+    }
+
+    public void go_Wbikini(View V){
+        Intent i = new Intent(this,women_panties1.class);
+        startActivity(i);
+        finish();
+    }
+
+    public void go_Wbelt(View V){
+        Intent i = new Intent(this,women_belt.class);
+        startActivity(i);
+        finish();
+    }
+
+    public void go_Wheels(View V){
+        Intent i = new Intent(this,women_shoes.class);
+        startActivity(i);
+        finish();
+    }
+
+    public void go_Wpregnant(View V){
+        Intent i = new Intent(this,women_maternity.class);
+        startActivity(i);
+        finish();
+    }
+
+    public void go_Wskirt(View V){
+        Intent i = new Intent(this,women_skirt.class);
+        startActivity(i);
+        finish();
+    }
+    public void go_Wdress(View V){
+        Intent i = new Intent(this,women_dress.class);
+        startActivity(i);
+        finish();
+    }
+    public void go_Wtights(View V){
+        Intent i = new Intent(this,women_tight.class);
+        startActivity(i);
+        finish();
+    }
+    public void go_Wring(View V){
+        Intent i = new Intent(this,women_ring.class);
+        startActivity(i);
+        finish();
+    }
+
+
+
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (keyCode == KeyEvent.KEYCODE_BACK ) {
