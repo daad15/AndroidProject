b5b67684f290d551cce32762364830ca579d671d
KadiAlrwaishdi <kadia654@gmail.com>
2022-05-09 12:03:06 +0300

    kids page

--- app/src/main/java/com/example/rakez/Kids.java
+++ app/src/main/java/com/example/rakez/Kids.java
@@ -20,6 +20,36 @@ public class Kids extends AppCompatActivity {
         startActivity(i);
         finish();
     }
+    public void go_beany(View V){
+        Intent i = new Intent(this,baby_beany.class);
+        startActivity(i);
+        finish();
+    }
+    public void go_boys(View V){
+        Intent i = new Intent(this,ch_boy.class);
+        startActivity(i);
+        finish();
+    }
+    public void go_girls(View V){
+        Intent i = new Intent(this,ch_girl.class);
+        startActivity(i);
+        finish();
+    }
+    public void go_Bshoes(View V){
+        Intent i = new Intent(this,ch_shoes.class);
+        startActivity(i);
+        finish();
+    }
+    public void go_onetoseven(View V){
+        Intent i = new Intent(this,toddler.class);
+        startActivity(i);
+        finish();
+    }
+    public void go_newborn(View V){
+        Intent i = new Intent(this,baby.class);
+        startActivity(i);
+        finish();
+    }
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (keyCode == KeyEvent.KEYCODE_BACK ) {
